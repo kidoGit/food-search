3caffcb084432ae6ae17454f2bcc9e9dc392729c
Poonamkumar Parekh <pkparekh95@gmail.com>
2020-05-09 18:31:50 +0530

    Completed search bar and search screen

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen
   },
-});
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
+
+// nI8BDds-G9U0SiLGDjtciw
+// khVfVymwqFVf1e1YppdwcRGCzMYY4NlauSLEvwT92z8ojkdQOwfuiDlooCVczXBcOlFuQslbx77P6ILD-BclTqnlDn04lbKWBT9I3TeuMT9stb8OnjbKeWBuN4m1XnYx
